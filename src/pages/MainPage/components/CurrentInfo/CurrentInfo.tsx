9db26ab7317b566d1b083847b09088fb73b189a8
Taump <taump@yandex.ru>
2024-07-19 14:44:29 +0400

    fix: styles

--- src/pages/MainPage/components/CurrentInfo/CurrentInfo.tsx
+++ src/pages/MainPage/components/CurrentInfo/CurrentInfo.tsx
@@ -89,7 +89,7 @@ export const CurrentInfo: React.FC<ICurrentInfo> = ({
 
       {issuer ? <>
         {issuer.isAa && <>
-          {issuer.homepage_url ? <div><b>Asset issuer website:</b>{" "}
+          {issuer.homepage_url ? <div className={styles.issuerWrap}><b>Asset issuer website:</b>{" "}
             <a target="_blank" rel="noopener" href={issuer?.meta?.viewLink || issuer.homepage_url}
             >
               {issuer?.meta?.viewLabel || issuer.description || issuer.address}
